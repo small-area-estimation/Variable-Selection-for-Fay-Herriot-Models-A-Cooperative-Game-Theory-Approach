9d1c68c14c137cb744079a083a3485dcfa2a6343
Small Area Estimation group <63600528+small-area-estimation@users.noreply.github.com>
2026-02-09 12:49:30 +0100

    Update use_example.R

--- use_example.R
+++ use_example.R
@@ -28,6 +28,7 @@ for(j in 1:(length(coal[[2]])-1)){
 }
 
 ### We create a list with the formulas to be used for each model.
+xd<-data_example[,5:7]
 comb_var <- lapply(all_comb, function(x) x <- as.data.frame(xd[,x]))
 comb_form_var <- lapply(comb_var, function(x) x <- as.formula(paste("yd ~ 0 +", paste(colnames(x), collapse= "+"))))
 
@@ -40,7 +41,7 @@ for(j in 1:nvar){
 
 
 ### We compute the model with all the explanatory variables
-full.model <- eblupFH(yd ~ 0 + X1 + X2 + X3 , vardir = sigma2d, data = datos)
+full.model <- eblupFH(yd ~ 0 + X1 + X2 + X3 , vardir = sigma2d, data = data_example)
 full.model
 
 
@@ -48,7 +49,7 @@ KIC <- rep(0, length(comb_var)) ## Create a empty list to save the KIC for each 
 
 for(i in seq_along(comb_var)){
   
-  model.i <- eblupFH(formula = comb_form_var[[i]], vardir = sigma2d, method = "ML", B = 200, data = datos) ## Fit the model
+  model.i <- eblupFH(formula = comb_form_var[[i]], vardir = sigma2d, method = "ML", B = 200, data = data_example) ## Fit the model
   
   KIC[i] <- model.i$fit$goodness[4] ## Save the KIC estimator
     
@@ -56,19 +57,20 @@ for(i in seq_along(comb_var)){
 }
 
 shapley_exact(KIC, nvar) ### Obtain the Shapley Value
+shapley(KIC, nvar) ### Obtain the Shapley Value
 
 
 #### Compute the nested models according to Shapley Value importance
 
-modelX2 <- eblupFH(yd ~ 0 + X1 , vardir = sigma2d, data = datos)
+modelX2 <- eblupFH(yd ~ 0 + X1 , vardir = sigma2d, data = data_example)
 modelX2$fit$goodness
 modelX2$fit$estcoef[,4]
 
-modelX21 <- eblupFH(yd ~ 0 + X1 + X2, vardir = sigma2d, data = datos)
+modelX21 <- eblupFH(yd ~ 0 + X1 + X2, vardir = sigma2d, data = data_example)
 modelX21$fit$goodness
 modelX21$fit$estcoef[,4]
 
-modelX213 <- eblupFH(yd ~ 0 + X1 + X2 + X3, vardir = sigma2d, data = datos)
+modelX213 <- eblupFH(yd ~ 0 + X1 + X2 + X3, vardir = sigma2d, data = data_example)
 modelX213$fit$goodness
 modelX213$fit$estcoef[,4]
 
